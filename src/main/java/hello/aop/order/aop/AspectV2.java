1c38a532479127739e71908581191acd8ece4d99
jemin <jemin03120111@gmail.com>
2022-02-16 22:52:56 +0900

    .

---
+++ src/main/java/hello/aop/order/aop/AspectV2.java
@@ -0,0 +1,24 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    // hello.aop.order 패키지와 하위 패키지
+   @Pointcut("execution(* hello.aop.order..*(..))")
+   private void allOrder(){} // pointcut signature
+
+    @Around("allOrder()") // 포인트컷
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable // 어드바이스
+    {
+        // joinPoint.getSignature
+        log.info("[log] {}",joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+}
